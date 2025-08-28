b2cfffc43b2544d7551e8c4ef1f28ae1de0cf629
seoboss-stack <dotcompublishing@gmail.com>
2025-08-29 08:54:40 +1200

    Update debug-env.js

--- netlify/functions/debug-env.js
+++ netlify/functions/debug-env.js
@@ -3,12 +3,8 @@ exports.handler = async () => {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      PUBLIC_HMAC_KEY: process.env.PUBLIC_HMAC_KEY ? "(set)" : "(missing)",
-      FORWARD_SECRET: process.env.FORWARD_SECRET ? "(set)" : "(missing)",
-      // bonus: show the ROUTE_MAP targets you care about
-      N8N_ONBOARD_SUBMIT_URL: process.env.N8N_ONBOARD_SUBMIT_URL ? "(set)" : "(missing)",
-      N8N_ONBOARD_ACTIVATE_URL: process.env.N8N_ONBOARD_ACTIVATE_URL ? "(set)" : "(missing)",
-      N8N_ONBOARD_RESEND_URL: process.env.N8N_ONBOARD_RESEND_URL ? "(set)" : "(missing)"
+      PUBLIC_HMAC_KEY: process.env.PUBLIC_HMAC_KEY || "(missing)",
+      FORWARD_SECRET: process.env.FORWARD_SECRET || "(missing)"
     })
   };
 };
