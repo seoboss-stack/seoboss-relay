526bd36782255cdedde822f3ba46c269eeb6f177
seoboss-stack <dotcompublishing@gmail.com>
2025-09-23 16:39:32 +1200

    Update debug-env.js

--- netlify/functions/debug-env.js
+++ netlify/functions/debug-env.js
@@ -1,10 +1,1 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      PUBLIC_HMAC_KEY: process.env.PUBLIC_HMAC_KEY || "(missing)",
-      FORWARD_SECRET: process.env.FORWARD_SECRET || "(missing)"
-    })
-  };
-};
+
